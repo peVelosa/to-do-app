b278162b41c4b99e75ff36c61b56941363c0d9a0
pedro.velosa <pedro.velosa@fiap.com.br>
2023-04-13 15:06:32 -0300

    type for production dev

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,7 +5,8 @@ import axios from "@/libs/axios";
 import useAuth from "@/utils/hooks/useAuth";
 import { ToDoType } from "@/types/Todo";
 
-async function fetchTodo({ user_Id }) {
+async function fetchTodo({ user_Id }: { user_Id: string | undefined }) {
+  if (!user_Id) return;
   return await axios
     .get("/todo", {
       params: {
@@ -17,12 +18,12 @@ async function fetchTodo({ user_Id }) {
 
 const Main = () => {
   const { user } = useAuth();
-  const { data } = useQuery<ToDoType>({
+  const { data } = useQuery<ToDoType | undefined>({
     queryKey: ["todos"],
     queryFn: () => fetchTodo({ user_Id: user?.id }),
     enabled: !!user,
   });
-
+  console.log(data);
   return <Container component={"main"}>Main</Container>;
 };
 
