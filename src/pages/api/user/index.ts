1b2ac707b25654df44fafbfafc506fc52046c911
pedro.velosa <pedro.velosa@fiap.com.br>
2023-04-13 16:56:46 -0300

    user/index change debug 8

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -1,31 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import getAuth from "@/utils/auth/getAuth";
 import createUser from "@/utils/user/createUser";
 import type { NextApiRequest, NextApiResponse } from "next";
-import prisma from "@/libs/prisma";
-import bcrypt from "bcrypt";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
     const { email, firstName, lastName, password } = req.body.req;
-    const numSaltRounds = 8;
-    const hashedPassword = await bcrypt.hash(password, numSaltRounds);
-
-    const data = await prisma.user.create({
-      data: {
-        email: "velosa2002@gmail.com",
-        username: `Pedro Velosa`,
-        password: "oi",
-      },
-    });
-    // await createUser({ email, firstName, lastName, password });
-    res.status(201).json(data);
-    // try {
-    //   await createUser({ email, firstName, lastName, password });
-    //   res.status(201).end();
-    // } catch {
-    //   res.status(202).json({ err: "Email already exists" });
-    // }
+    try {
+      await createUser({ email, firstName, lastName, password });
+      res.status(201).end();
+    } catch {
+      res.status(202).json({ err: "Email already exists" });
+    }
   }
 }
